027be6b8d836c3d70788b406ef64471b96ae40b2
Logan Rosen <loganrosen@gmail.com>
2014-10-25 23:36:07 -0400

    set up users

--- Gemfile
+++ Gemfile
@@ -28,6 +28,8 @@ gem 'spring',        group: :development
 
 gem 'rspec-rails', group: [:development, :test]
 
+gem 'devise'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -75,4 +75,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Add default URL options for Devise mailer
+  config.action_mailer.default_url_options = { host: 'onthedocket.heroku.com' }
 end
